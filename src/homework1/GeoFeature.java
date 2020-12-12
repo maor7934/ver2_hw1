cd2615c5d97f75f34ae328134a417486444c1b09
Maor <maor7934@gmail.com>
2020-12-12 13:20:09 +0200

    first commit

---
+++ src/homework1/GeoFeature.java
@@ -0,0 +1,266 @@
+package homework1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * A GeoFeature represents a route from one location to another along a single
+ * geographic feature. GeoFeatures are immutable.
+ * <p>
+ * GeoFeature abstracts over a sequence of GeoSegments, all of which have the
+ * same name, thus providing a representation for nonlinear or nonatomic
+ * geographic features. As an example, a GeoFeature might represent the course
+ * of a winding river, or travel along a road through intersections but
+ * remaining on the same road.
+ * <p>
+ * GeoFeatures are immutable. New GeoFeatures can be constructed by adding a
+ * segment to the end of a GeoFeature. An added segment must be properly
+ * oriented; that is, its p1 field must correspond to the end of the original
+ * GeoFeature, and its p2 field corresponds to the end of the new GeoFeature,
+ * and the name of the GeoSegment being added must match the name of the
+ * existing GeoFeature.
+ * <p>
+ * Because a GeoFeature is not necessarily straight, its length - the distance
+ * traveled by following the path from start to end - is not necessarily the
+ * same as the distance along a straight line between its endpoints.
+ * <p>
+ * <b>The following fields are used in the specification:</b>
+ * 
+ * <pre>
+ *   start : GeoPoint       // location of the start of the geographic feature
+ *   end : GeoPoint         // location of the end of the geographic feature
+ *   startHeading : angle   // direction of travel at the start of the geographic feature, in degrees
+ *   endHeading : angle     // direction of travel at the end of the geographic feature, in degrees
+ *   geoSegments : sequence	// a sequence of segments that make up this geographic feature
+ *   name : String          // name of geographic feature
+ *   length : real          // total length of the geographic feature, in kilometers
+ * </pre>
+ **/
+public class GeoFeature {
+
+	// Implementation hint:
+	// When asked to return an Iterator, consider using the iterator() method
+	// in the List interface. Two nice classes that implement the List
+	// interface are ArrayList and LinkedList. If comparing two Lists for
+	// equality is needed, consider using the equals() method of List. More
+	// info can be found at:
+	// http://docs.oracle.com/javase/8/docs/api/java/util/List.html
+
+	final GeoPoint start;
+	final GeoPoint end;
+	final double length;
+	final double startHeading;
+	final double endHeading;
+	final ArrayList<GeoSegment> geoSegments;
+	final String name;
+
+	// TODO Write abstraction function and representation invariant
+	private void checkRep() {
+    	assert this.geoSegments.size() != 0 : "GeoFeature RI violated: no segments";
+    	assert this.name != null : "GeoFeature RI vioalted: name is null";
+    	assert !this.start.equals(this.end) : "GeoFeature RI violated - start equals end";
+    	GeoSegment first, last = null;
+    	for (iterator<GeoSegment> iter = this.geoSegments.iterator(); it.hasNext();) {
+    		last = first;
+    		first = iter.next();
+			assert this.name.equals(first.getName()) :
+    			"GeoFeature RI violated :  segment does not meet req (name does not match).";
+			if (last == null) {
+				 assert this.start.equals(first.getP1()) : 
+					 "GeoFeature RI violated : first segment p1 != start.";
+			}
+			else {
+				assert last.getP2().equals(first.getP1()):
+					"GeoFeature RI violated"
+			}
+			if (!iter.hasNext()) {
+				assert first.getP2().equals(this.end):
+					"GeoFeature RI violated : last segment p2 != end.";
+			}
+				    				
+    	}
+    }
+
+	/**
+	 * Constructs a new GeoFeature.
+	 * 
+	 * @requires gs != null
+	 * @effects Constructs a new GeoFeature, r, such that r.name = gs.name &&
+	 *          r.startHeading = gs.heading && r.endHeading = gs.heading && r.start
+	 *          = gs.p1 && r.end = gs.p2
+	 **/
+	public GeoFeature(GeoSegment gs) {
+		assert gs != null : "GeoFeature C'tor : gs received is null";
+		this.name = gs.getName();
+		this.startHeading = gs.getHeading();
+		this.endHeading = gs.getHeading();
+		this.start = gs.getP1();
+		this.end = gs.getP2();
+		this.length = gs.getLength();
+		this.geoSegments = new ArrayList<GeoSegment>();
+		this.geoSegments.add(gs);
+		this.checkRep();
+	}
+	/**
+	 * Constructs a new GeoFeature from an exisiting GeoFeature and a new GeoSegment
+	 * @requires gf != null %%  gs != null && gs.name == gf.name
+	 * @effects Constructs a new GeoFeature, r, such that r.name = gf.name &&
+	 *          r.startHeading = gf.heading && r.endHeading = gs.heading && r.start
+	 *          = gf.p1 && r.end = gs.p2
+	 **/
+	public GeoFeature(GeoFeature gf, GeoSegment gs) {
+	this.name = gf.getName();
+	this.startHeading = gf.getStartHeading();
+	this.endHeading = gs.getHeading();
+	this.start = gf.getStart();
+	this.end = gs.getP2();
+	this.length = gf.getLength() + gs.getLength();
+	this.geoSegments = new ArrayList<GeoSegment>(gf.geoSegments); // hard copy
+	this.geoSegments.add(gs);
+	this.checkRep();
+	}
+	
+	/**
+	 * Returns name of geographic feature.
+	 * 
+	 * @return name of geographic feature
+	 */
+	public String getName() {
+		this.checkRep();
+		return this.name;
+	}
+
+	/**
+	 * Returns location of the start of the geographic feature.
+	 * 
+	 * @return location of the start of the geographic feature.
+	 */
+	public GeoPoint getStart() {
+		this.checkRep();
+		return this.start;
+	}
+
+	/**
+	 * Returns location of the end of the geographic feature.
+	 * 
+	 * @return location of the end of the geographic feature.
+	 */
+	public GeoPoint getEnd() {
+		this.checkRep();
+		return this.end;
+	}
+
+	/**
+	 * Returns direction of travel at the start of the geographic feature.
+	 * 
+	 * @return direction (in standard heading) of travel at the start of the
+	 *         geographic feature, in degrees.
+	 */
+	public double getStartHeading() {
+		this.checkRep();
+		return this.startHeading;
+	}
+
+	/**
+	 * Returns direction of travel at the end of the geographic feature.
+	 * 
+	 * @return direction (in standard heading) of travel at the end of the
+	 *         geographic feature, in degrees.
+	 */
+	public double getEndHeading() {
+		this.checkRep();
+		return this.endHeading;
+	}
+
+	/**
+	 * Returns total length of the geographic feature, in kilometers.
+	 * 
+	 * @return total length of the geographic feature, in kilometers. NOTE: this is
+	 *         NOT as-the-crow-flies, but rather the total distance required to
+	 *         traverse the geographic feature. These values are not necessarily
+	 *         equal.
+	 */
+	public double getLength() {
+		this.checkRep();
+		return this.length()
+	}
+
+	/**
+	 * Creates a new GeoFeature that is equal to this GeoFeature with gs appended to
+	 * its end.
+	 * 
+	 * @requires gs != null && gs.p1 = this.end && gs.name = this.name.
+	 * @return a new GeoFeature r such that r.end = gs.p2 && r.endHeading =
+	 *         gs.heading && r.length = this.length + gs.length
+	 **/
+	public GeoFeature addSegment(GeoSegment gs) {
+		this.checkRep();
+		assert gs!= null : "GeoFeature addSegment : gs is null";
+		assert this.end.equals(gs.getP1()) : : "GeoFeature addSegment : end != gs.P1";
+		assert this.name.equals((gs.getName()) : "GeoFeature addSegment : gs name doesnt match feature";
+		try {
+			return new GeoFeature(this, gf);
+		}
+		finally {
+			this.checkRep();
+		}
+	}
+
+	/**
+	 * Returns an Iterator of GeoSegment objects. The concatenation of the
+	 * GeoSegments, in order, is equivalent to this GeoFeature. All the GeoSegments
+	 * have the same name.
+	 * 
+	 * @return an Iterator of GeoSegments such that
+	 * 
+	 *         <pre>
+	 *      this.start        = a[0].p1 &&
+	 *      this.startHeading = a[0].heading &&
+	 *      this.end          = a[a.length - 1].p2 &&
+	 *      this.endHeading   = a[a.length - 1].heading &&
+	 *      this.length       = sum(0 <= i < a.length) . a[i].length &&
+	 *      for all integers i
+	 *          (0 <= i < a.length-1 => (a[i].name == a[i+1].name &&
+	 *                                   a[i].p2d  == a[i+1].p1))
+	 *         </pre>
+	 * 
+	 *         where <code>a[n]</code> denotes the nth element of the Iterator.
+	 * @see homework1.GeoSegment
+	 */
+	public Iterator<GeoSegment> getGeoSegments() {
+		this.checkRep();
+		return this.getGeoSegments().iterator();
+	}
+
+	/**
+	 * Compares the argument with this GeoFeature for equality.
+	 * 
+	 * @return o != null && (o instanceof GeoFeature) && (o.geoSegments and
+	 *         this.geoSegments contain the same elements in the same order).
+	 **/
+	public boolean equals(Object o) {
+		this.checkRep();
+		return o != null && o instance of GeoFeature &&
+				((GeoFeature)o).getGeoSegments().equals((this.geoSegments));
+	}
+
+	/**
+	 * Returns a hash code for this.
+	 * 
+	 * @return a hash code for this.
+	 **/
+	public int hashCode() {
+		this.checkRep();
+		return this.start.hashCode()+this.end.hashCode + (int)this.length;
+	}
+
+	/**
+	 * Returns a string representation of this.
+	 * 
+	 * @return a string representation of this.
+	 **/
+	public String toString() {
+		this.checkRep();
+		return this.name; //TODO MAKE THIS MORE INFORMATIVE IMPORTANT!!
+	}
+}
---
+++ src/homework1/GeoPoint.java
@@ -0,0 +1,217 @@
+package homework1;
+
+/**
+ * A GeoPoint is a point on the earth. GeoPoints are immutable.
+ * <p>
+ * North latitudes and east longitudes are represented by positive numbers.
+ * South latitudes and west longitudes are represented by negative numbers.
+ * <p>
+ * The code may assume that the represented points are nearby the Technion.
+ * <p>
+ * <b>Implementation direction</b>:<br>
+ * The Ziv square is at approximately 32 deg. 46 min. 59 sec. N
+ * latitude and 35 deg. 0 min. 52 sec. E longitude. There are 60 minutes
+ * per degree, and 60 seconds per minute. So, in decimal, these correspond
+ * to 32.783098 North latitude and 35.014528 East longitude. The 
+ * constructor takes integers in millionths of degrees. To create a new
+ * GeoPoint located in the the Ziv square, use:
+ * <tt>GeoPoint zivCrossroad = new GeoPoint(32783098,35014528);</tt>
+ * <p>
+ * Near the Technion, there are approximately 110.901 kilometers per degree
+ * of latitude and 93.681 kilometers per degree of longitude. An
+ * implementation may use these values when determining distances and
+ * headings.
+ * <p>
+ * <b>The following fields are used in the specification:</b>
+ * <pre>
+ *   latitude :  real        // latitude measured in degrees
+ *   longitude : real        // longitude measured in degrees
+ * </pre>
+ **/
+public class GeoPoint {
+
+	/** Minimum value the latitude field can have in this class. **/
+	public static final int MIN_LATITUDE  =  -90 * 1000000;
+	    
+	/** Maximum value the latitude field can have in this class. **/
+	public static final int MAX_LATITUDE  =   90 * 1000000;
+	    
+	/** Minimum value the longitude field can have in this class. **/
+	public static final int MIN_LONGITUDE = -180 * 1000000;
+	    
+	/** Maximum value the longitude field can have in this class. **/
+	public static final int MAX_LONGITUDE =  180 * 1000000;
+
+  	/**
+   	 * Approximation used to determine distances and headings using a
+     * "flat earth" simplification.
+     */
+  	public static final double KM_PER_DEGREE_LATITUDE = 110.901;
+
+  	/**
+     * Approximation used to determine distances and headings using a
+     * "flat earth" simplification.
+     */
+  	public static final double KM_PER_DEGREE_LONGITUDE = 93.681;
+  	
+  	/**
+  	 * integer used too normalize calcualtions
+  	 */
+  	public static final int ONE_MILLION = 1000000;
+	// Implementation hint:
+	// Doubles and floating point math can cause some problems. The exact
+	// value of a double can not be guaranteed except within some epsilon.
+	// Because of this, using doubles for the equals() and hashCode()
+	// methods can have erroneous results. Do not use floats or doubles for
+	// any computations in hashCode(), equals(), or where any other time 
+	// exact values are required. (Exact values are not required for length 
+	// and distance computations). Because of this, you should consider 
+	// using ints for your internal representation of GeoPoint. 
+
+  	
+  	final int latitude_int;
+  	final int longtitude_int;
+  	
+  	/* Checks the rep. Inv.
+  	 * @throws Assertion error if violated. 
+  	 */
+  	private void checkRep () {
+  		assert 
+  		this.latitude_int <= MAX_LATITUDE and this.latitude_int >=MIN_LATITUDE
+  				and this.longtitude <= MAX_LATITUDE and this.longtitude >=MIN_LONGITUDE:
+  					"Rep. Inv. of class GeoPoint is violated.";
+  	}
+  	
+  	
+  	/**
+  	 * Constructs GeoPoint from a latitude and longitude.
+     * @requires the point given by (latitude, longitude) in millionths
+   	 *           of a degree is valid such that:
+   	 *           (MIN_LATITUDE <= latitude <= MAX_LATITUDE) and
+     * 	 		 (MIN_LONGITUDE <= longitude <= MAX_LONGITUDE)
+   	 * @effects constructs a GeoPoint from a latitude and longitude
+     *          given in millionths of degrees.
+   	 **/
+  	public GeoPoint(int latitude, int longitude) {
+  		this.latitude_int = latitude;
+  		this.longtitude_int = longitude;
+  		this.checkRep();
+  		
+  	}
+
+  	 
+  	/**
+     * Returns the latitude of this.
+     * @return the latitude of this in millionths of degrees.
+     */
+  	public int getLatitude() {
+  		this.checkRep();
+  		return this.latitude_int;
+  	}
+
+
+  	/**
+     * Returns the longitude of this.
+     * @return the latitude of this in millionths of degrees.
+     */
+  	public int getLongitude() {
+  		this.checkRep();
+  		return this.longtitude_int;
+  	}
+
+
+  	/**
+     * Computes the distance between GeoPoints.
+     * @requires gp != null
+     * @return the distance from this to gp, using the flat-surface, near
+     *         the Technion approximation.
+     **/
+  	public double distanceTo(GeoPoint gp) {
+  		this.checkRep();
+  		gp.checkRep();
+  		try {
+  			// Exact values not required, can use doubles
+  			double y_distance = ((this.latitude_int - gp.latitude_int)/ONE_MILLION)*KM_PER_DEGREE_LATITUDE;
+  			double x_distance = ((this.longtitude_int - gp.longtitude_int)/ONE_MILLION)*KM_PER_DEGREE_LONGITUDE;
+  			double total_distance = Math.sqrt(Math.pow(x_distance, 2)  + Math.pow(y_distance, 2));
+  			return total_distance;
+  			
+  		}
+  		 finally {
+  			 gp.checkRep();
+  			 this.checkRep();
+  		 }
+  	}
+
+  	/**
+     * Computes the compass heading between GeoPoints.
+     * @requires gp != null && !this.equals(gp)
+     * @return the compass heading h from this to gp, in degrees, using the
+     *         flat-surface, near the Technion approximation, such that
+     *         0 <= h < 360. In compass headings, north = 0, east = 90,
+     *         south = 180, and west = 270.
+     **/
+  	public double headingTo(GeoPoint gp) {
+		// Implementation hints:
+		// 1. You may find the mehtod Math.atan2() useful when
+		// implementing this method. More info can be found at:
+		// http://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
+		//
+		// 2. Keep in mind that in our coordinate system, north is 0
+		// degrees and degrees increase in the clockwise direction. By
+		// mathematical convention, "east" is 0 degrees, and degrees
+		// increase in the counterclockwise direction.
+
+		// TODO Implement this method
+		this.checkRep();
+		gp.checkRep();
+		assert !this.equals(gp) : "headingTo got 2 identical points";
+		assert gp != null : "headingTo got null gp";
+		try {
+			double relative_y = (this.latitude_int - gp.latitude_int) / ONE_MILLION * KM_PER_DEGREE_LATITUDE;
+			double relative_x = (this.longtitude_int - gp.longtitude_int) / ONE_MILLION * KM_PER_DEGREE_LATITUDE;
+			double radian = - Math.atan2(relative_y, relative_x);
+			double degree = Math.toDegrees(radian) + 90; // TODO: move to const
+			return degree;
+		} finally {
+			this.checkRep();
+			gp.checkRep();
+		}
+	}
+
+	/**
+	 * Compares the specified Object with this GeoPoint for equality.
+	 * 
+	 * @return gp != null && (gp instanceof GeoPoint) && gp.latitude = this.latitude
+	 *         && gp.longitude = this.longitude
+	 **/
+	public boolean equals(Object gp) {
+		this.checkRep();
+		try {
+			return gp != null && gp instanceof GeoPoint && ((GeoPoint) gp).latitude_int == this.latitude_int
+					&& ((GeoPoint) gp).longtitude_int == this.longtitude_int;
+		} finally {
+			this.checkRep();
+		}
+
+	}
+
+	/**
+	 * Returns a hash code value for this GeoPoint.
+	 * 
+	 * @return a hash code value for this GeoPoint.
+	 **/
+	public int hashCode() {
+		this.checkRep();
+		return this.latitude_int * 2 + this.longtitude_int;
+	}
+
+	/**
+	 * Returns a string representation of this GeoPoint.
+	 * 
+	 * @return a string representation of this GeoPoint.
+	 **/
+	public String toString() {
+		this.checkRep();
+		return String.format("(latitude,longitude) = (%d,%d)",this.latitude,this.longitude);
+	}
